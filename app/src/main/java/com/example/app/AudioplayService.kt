37676f35f036208190f9f8cd45d99a633c6dd1d1
Cristian-hash <cpacori@unsa.edu.pe>
2024-07-10 19:30:06 -0500

    Creacion de los comandos

--- app/src/main/java/com/example/app/AudioplayService.kt
+++ app/src/main/java/com/example/app/AudioplayService.kt
@@ -5,13 +5,32 @@ import android.content.Intent
 import android.os.IBinder
 
 class AudioplayService: Service() {
+    //iniciar el audio
+    companion object{
+
+        const val FILENAME="FILENAME"
+        const val COMMAND="COMMAND"
+        const val PLAY="PLAY"
+        const val STOP="STOP"
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
 
 
     override fun onStartCommand(intent: Intent,flags:Int,startId:Int):Int{
+        val filename = intent.getStringExtra(FILENAME)
+        val command = intent.getStringExtra(COMMAND)
+
+        if(command == PLAY){
+
+        }else if(command == STOP){
+
+        }
 
         return START_STICKY
     }
+
+    private fun audio
 }
