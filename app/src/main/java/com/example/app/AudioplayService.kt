b4df59c5243954b655a6e05eaabff13d6a1c9d66
Cristian-hash <cpacori@unsa.edu.pe>
2024-07-10 19:45:49 -0500

    metodo de audioPlay

--- app/src/main/java/com/example/app/AudioplayService.kt
+++ app/src/main/java/com/example/app/AudioplayService.kt
@@ -2,10 +2,14 @@ package com.example.app
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 
 class AudioplayService: Service() {
     //iniciar el audio
+    private lateinit val mediaPlayer:MediaPlayer
+
+
     companion object{
 
         const val FILENAME="FILENAME"
@@ -32,5 +36,22 @@ class AudioplayService: Service() {
         return START_STICKY
     }
 
-    private fun audio
+    private fun audioPlay(filename:String){
+            if(filename!=null){
+                val assetFileDescriptor = assets.openFd(filename)
+
+                mediaPlayer=MediaPlayer()
+                mediaPlayer.setDataSource(
+                    assetFileDescriptor.fileDescriptor,
+                    assetFileDescriptor.startOffset,
+                    assetFileDescriptor.length
+                )
+
+                assetFileDescriptor.close()
+                mediaPlayer.prepare()
+                mediaPlayer.setVolume(1f,1f)
+                mediaPlayer.isLooping=false
+                mediaPlayer.start()
+            }
+    }
 }
